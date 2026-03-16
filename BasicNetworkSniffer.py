647767c59276caaf785631175e0cf5e6266d90a7
saranyakanikaram <kdevisaranya06@gmail.com>
2026-03-16 19:52:58 +0530

    Implement packet analysis in BasicNetworkSniffer
    Added packet analysis functionality to capture and display source and destination IPs, protocol type, and payload for TCP and UDP packets.

--- BasicNetworkSniffer.py
+++ BasicNetworkSniffer.py
@@ -1,1 +1,35 @@
+from scapy.all import sniff, IP, TCP, UDP, Raw
 
+# Function to analyze packets
+def analyze_packet(packet):
+
+    if packet.haslayer(IP):
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        protocol = packet[IP].proto
+
+        print("\n--- Packet Captured ---")
+        print("Source IP:", src_ip)
+        print("Destination IP:", dst_ip)
+        print("Protocol:", protocol)
+
+        # Check for TCP
+        if packet.haslayer(TCP):
+            print("Protocol Type: TCP")
+            print("Source Port:", packet[TCP].sport)
+            print("Destination Port:", packet[TCP].dport)
+
+        # Check for UDP
+        elif packet.haslayer(UDP):
+            print("Protocol Type: UDP")
+            print("Source Port:", packet[UDP].sport)
+            print("Destination Port:", packet[UDP].dport)
+
+        # Payload
+        if packet.haslayer(Raw):
+            print("Payload:", packet[Raw].load)
+
+# Capture packets
+print("Starting packet capture... Press Ctrl+C to stop")
+
+sniff(prn=analyze_packet, count=10)
